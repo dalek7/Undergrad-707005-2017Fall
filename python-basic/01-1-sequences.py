e64796a939f816376b0a0bba6dd4b8b0386c76dc
seung <ncraig@naver.com>
2017-09-11 11:58:16 +0900

    error fix

--- python-basic/01-1-sequences.py
+++ python-basic/01-1-sequences.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+aaaaaaaaaaaaasdasdasdasdasda
 # creating a tuple
 months = ('January','February','March','April','May','June',\
 'July','August','September','October','November','  December')
