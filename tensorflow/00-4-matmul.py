c188f75274a1258e5d8f731aa6b7a2b4fa0dc9b2
Seung <seung@Seungs-MacBook-Pro.local>
2017-09-13 14:01:40 +0900

    updates for python 3

--- tensorflow/00-4-matmul.py
+++ tensorflow/00-4-matmul.py
@@ -9,10 +9,13 @@ y2 = tf.constant([[1.0, 1.0], [0.0, 1.0]])
 z2 = tf.matmul(x2, y2)
 
 with tf.Session() as session:
-	result = session.run(y, feed_dict={x: [1, 2, 3]})
-	print(result)
-	
-	z = session.run(z2, feed_dict={x2: [[3.0, 4.0], [5.0, 6.0]]})
-	print z2
-	print z
+    result = session.run(y, feed_dict={x: [1, 2, 3]})
+    print(result)
+    result = session.run(y, feed_dict={x: [2, 5, -1]})
+    print(result)
+    print("----------------------------")
+    z = session.run(z2, feed_dict={x2: [[3.0, 4.0], [5.0, 6.0]]})
+    print(z2)
+    print("----------------------------")
+    print(z)
 	
