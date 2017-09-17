7382f6a4ce9aefe63189622a04f84ef4a7279b82
seung <ncraig@naver.com>
2017-09-17 10:37:35 +0900

    linear regression

--- tensorflow/01-1-linear_regression.py
+++ tensorflow/01-1-linear_regression.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 717005
 import tensorflow as tf
 tf.set_random_seed(777)  # for reproducibility
@@ -6,12 +7,15 @@ import matplotlib.pyplot as plt
 
 # X and Y data
 x_train = [1, 2, 3]
-y_train = [2, 4, 6]
+#y_train = [2, 4, 6] # 그냥 x_train 에 2배 곱해서 생성
+y_train = [3, 5, 7]
+
 #y_train = [2+0.1, 4-0.3, 6+0.15]
 
 # Try to find values for W and b to compute y_data = x_data * W + b
 # We know that W should be 2 and b should be 0
 # But let TensorFlow figure it out
+
 W = tf.Variable(tf.random_normal([1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
@@ -31,8 +35,7 @@ sess = tf.Session()
 # Initializes global variables in the graph.
 sess.run(tf.global_variables_initializer())
 
-vcost =[]
-
+vcost =[] # 비어있는 array (list)
 
 # Fit the line
 x1 =np.linspace(np.min(x_train)-1, np.max(x_train)+1, num=5)
@@ -50,16 +53,14 @@ for step in range(2001):
 
 plt.figure(1)
 plt.plot(x_train, y_train,'o')
-plt.plot(x1,m1*x1 + b1); plt.grid();
+plt.plot(x1,m1*x1 + b1); plt.grid()
 plt.grid()
 plt.axis((np.min(x_train) - 1, np.max(x_train) + 1, np.min(y_train) - 1, np.max(y_train) + 1))
-
 
 plt.figure(2)
 plt.plot(vcost)
 plt.grid()
 plt.show()
-
 
 # Learns best fit W:[ 2.],  b:[ 0.]
 
