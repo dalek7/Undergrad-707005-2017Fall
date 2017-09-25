2a0a8fa6efa8215cb89150b8698c280926c6992a
seung <ncraig@naver.com>
2017-09-25 14:05:39 +0900

    cleaned up

--- tensorflow/00-4-matmul.py
+++ tensorflow/00-4-matmul.py
@@ -3,15 +3,17 @@
 # placeholder를 이용한 행렬 연산
 import tensorflow as tf
 
-x  = tf.placeholder("float", None)
+x = tf.placeholder("float", None)
 x2 = tf.placeholder(tf.float32, shape=[2, 2])
 
-y = x * 2
+y1 = x * 2
 
+c1 = tf.constant([[1.0, 1.0], [0.0, 1.0]])
+y2 = tf.matmul(x2, c1)
 
 with tf.Session() as session:
-    result = session.run(y, feed_dict={x: [1, 2, 3]})
+    result = session.run(y1, feed_dict={x: [1, 2, 3]})
     print(result)
     print('')
-    result = session.run(y, feed_dict={x: [2, 5, -1]})
+    result = session.run(y2, feed_dict={x2: [[3.0, 4.0], [5.0, 6.0]]})
     print(result)
